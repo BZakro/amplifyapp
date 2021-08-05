3824b175cb8dfb4c8157e3d912cfe23c4f3f28be
BZakro <bennyzakro@gmail.com>
2021-08-05 12:20:54 +0300

    added api

--- src/App copy.js
+++ src/App copy.js
@@ -15,15 +15,6 @@ function App() {
     fetchNotes();
   }, []);
 
-  // handle the image upload:
-  async function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file.name });
-    await Storage.put(file.name, file);
-    fetchNotes();
-  }
-
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
@@ -56,21 +47,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value })}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value })}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <input
-        type="file"
-        onChange={onChange}
-      />
+      <h1>My User Page</h1>
+
       <button onClick={createNote}>Create Note</button>
       <div style={{ marginBottom: 30 }}>
         {
@@ -86,54 +64,9 @@ function App() {
           ))
         }
       </div>
-      <AmplifySignOut />
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
 
-export default withAuthenticator(App);
-
-
-// ////////////////////
-// Before installing AppSync API, DynamoDB table & 
-// local GraphQL operations:
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header>
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <h1>We now have Auth!</h1>
-//       </header>
-//       <AmplifySignOut />
-//     </div>
-//   );
-// }
-
-// export default withAuthenticator(App);
-
-
-// ////////////////////
-
-// Before using Amplify Auth:
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>Hello Benny</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
